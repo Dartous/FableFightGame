b914b451484a051998a4659411522083efeaa883
Taras Voloshyn <157302769+Dartous@users.noreply.github.com>
2025-04-15 11:31:58 +0100

    Feat: Unlock new lanes at wave 3 and 6

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -10,7 +10,11 @@ public class WaveManager : MonoBehaviour
     [Header("Add these from MOST powerful to LEAST powerful (measured by according enemyStrength)")]
     public GameObject[] enemies;
     public int[] enemyStrength;
-    public GameObject[] spawnPositions;
+    public GameObject[] spawnPositionsWave1;
+    public GameObject[] spawnPositionsWave3;
+    public GameObject[] spawnPositionsWave6;
+    private GameObject[] currentSpawnPositions;
+    public int waveCounter;
 
     [Header("Wave Stats")]
     public float firstSpawnDelay = 2;
@@ -24,17 +28,17 @@ public class WaveManager : MonoBehaviour
     [Header("For TESTING only")]
     public float currentSpawnRate;
     public int currentWaveStrength;
-    //public GameObject knight;
-    //public GameObject peasant;
-    //public GameObject blacksmith;
-    //public GameObject dog;
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
+
+        //initiate stuff
         currentWaveStrength = startingWaveStrength;
         currentSpawnRate = spawnRate;
+        waveCounter = 1;
+        currentSpawnPositions = spawnPositionsWave1;
 
         InvokeRepeating("SpawnEnemies", firstSpawnDelay, currentSpawnRate);
     }
@@ -56,42 +60,32 @@ public class WaveManager : MonoBehaviour
                 }
 
                 int randomAmount = 0;
-                //try 
-                //{
-                    //calculate a random amount of enemies, fitting within the set wave strength
-                    randomAmount = Random.Range(0, currentWaveStrength / enemyStrength[i]);
+
+                //calculate a random amount of enemies, fitting within the set wave strength
+                randomAmount = Random.Range(0, currentWaveStrength / enemyStrength[i]);
 
                 if (i == enemies.Length - 1)
                 {
                     randomAmount = tempWaveStrength;
                 }
-                //}
-                //catch
-                //{
-                //    print("enemyStr is more than tempWaveStr");
-                //}
 
-                //if (randomAmount > 0) 
-                //{
-                    //reduce the tempWaveStrength according to the number of enemies summoned
-                    tempWaveStrength -= randomAmount * enemyStrength[i];
+                //reduce the tempWaveStrength according to the number of enemies summoned
+                tempWaveStrength -= randomAmount * enemyStrength[i];
 
-                    //for each enemy
-                    for (int j = 0; j < randomAmount; j++)
-                    {
-                        //select a random spawn location
-                        int randomSpawn = Random.Range(0, spawnPositions.Length);
+                //for each enemy
+                for (int j = 0; j < randomAmount; j++)
+                {
+                    //select a random spawn location
+                    int randomSpawn = Random.Range(0, currentSpawnPositions.Length);
 
-                        //randomize Z coordinate in order for enemies to not spawn in the same pixel
-                        Vector3 spawnPos = spawnPositions[randomSpawn].transform.position;
-                        float randomZ = Random.Range(spawnPositions[randomSpawn].transform.position.z - spawnPositionSpread, spawnPositions[randomSpawn].transform.position.z + spawnPositionSpread);
-                        Vector3 newSpawnPos = new Vector3(spawnPos.x, spawnPos.y, randomZ);
+                    //randomize Z coordinate in order for enemies to not spawn in the same pixel
+                    Vector3 spawnPos = currentSpawnPositions[randomSpawn].transform.position;
+                    float randomZ = Random.Range(currentSpawnPositions[randomSpawn].transform.position.z - spawnPositionSpread, currentSpawnPositions[randomSpawn].transform.position.z + spawnPositionSpread);
+                    Vector3 newSpawnPos = new Vector3(spawnPos.x, spawnPos.y, randomZ);
 
-                        //instantiate an appropriate enemy there
-                        Instantiate(enemies[i], newSpawnPos, enemies[i].transform.rotation);
-                    }
-                //}
-                
+                    //instantiate an appropriate enemy there
+                    Instantiate(enemies[i], newSpawnPos, enemies[i].transform.rotation);
+                }
             }
         }
 
@@ -107,7 +101,18 @@ public class WaveManager : MonoBehaviour
             currentSpawnRate = minSpawnRate;
         }
 
+        if (waveCounter == 3)
+        {
+            currentSpawnPositions = spawnPositionsWave3;
+        }
+
+        if (waveCounter == 6)
+        {
+            currentSpawnPositions = spawnPositionsWave6;
+        }
+
         //increase wave strength
         currentWaveStrength += waveStrengthIncrease;
+        waveCounter++;
     }
 }
